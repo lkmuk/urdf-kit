4fa43c78919afcd74e7c5a3ba93de9c5f0d1fae1
Laikan Muk <lkmuk2017@gmail.com>
2022-10-25 16:04:12 +0200

    should be Python 3.9+ instead!

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     author="Lai Kan Muk",
     author_email="lkmuk2017@gmail.com",
     description="Useful addons for URDF editing, particularly conceived for postprocessing with Onshape-to-Robot's URDF export.",
-    python_requires='>=3.6',
+    python_requires='>=3.9',
     packages=setuptools.find_packages(),
     # install_requires=[
     #     "onshape-to-robot=0.3.17" # I guess it's not a straight requirements
