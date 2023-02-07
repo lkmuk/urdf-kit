25baebffa04c1445d0f862f78d8121cd20b7b0e9
L. Muk <lkmuk2017@gmail.com>
2023-02-07 13:27:10 +0100

    further enhancement to the fixed joint reduction demo

--- tests/advanced/graph/test_params.py
+++ tests/advanced/graph/test_params.py
@@ -1,67 +1,18 @@
 from urdf_kit.graph import params as robot_params
 import numpy as np
 import yaml
+from pathlib import Path
 
-EXPECTED_YAML = """
-robot_name: dummy
-joints:
-- joint_name: eta1
-  home_pose:
-  - - 1.0
-    - 0.0
-    - 0.0
-    - 0.0
-  - - 0.0
-    - 1.0
-    - 0.0
-    - 0.0
-  - - 0.0
-    - 0.0
-    - 1.0
-    - 0.0
-  - - 0.0
-    - 0.0
-    - 0.0
-    - 1.0
-  screw_axis:
-  - 1
-  - 0
-  - 0
-  - 0
-  - 0
-  - 1
-  parent_link_name: base
-  child_link_name: link1
-- joint_name: eta2
-  home_pose:
-  - - 1.0
-    - 0.0
-    - 0.0
-    - 0.0
-  - - 0.0
-    - 1.0
-    - 0.0
-    - 0.0
-  - - 0.0
-    - 0.0
-    - 1.0
-    - 0.0
-  - - 0.0
-    - 0.0
-    - 0.0
-    - 1.0
-  screw_axis:
-  - 1
-  - 0
-  - 0
-  - 0
-  - 0
-  - 1
-  parent_link_name: link1
-  child_link_name: link2
+def test_compositional_asdict(generate_oracle = False):
     """
+    TODO split up this test
+    """
+    expected_res_fpath = Path(__file__).parent/".."/".."/"test_output_expected"/"extracted_params_for_dummyBot.yaml"
+    if not generate_oracle:
+        with open(expected_res_fpath,"r") as f:
+            expected_dict = yaml.safe_load(f)
 
-def test_compositional_asdict():
+    # actually also SUTs....
     joint_mdls = [
         robot_params.joint_body_kinematics_param(
             "eta1", home_pose=np.eye(4), screw_axis=[1, 0, 0, 0, 0, 1], parent_link_name="base", child_link_name="link1"
@@ -71,14 +22,19 @@ def test_compositional_asdict():
         )
     ]
 
-    robot_mdl = robot_params.robot_kinematics(
-        robot_name="dummy", joints=joint_mdls)
+    # the real SUT
+    robot_mdl = robot_params.robot_kinematics(robot_name="dummy", joints=joint_mdls)
 
-    assert robot_mdl.as_dict() == yaml.safe_load(EXPECTED_YAML)
+    if not generate_oracle:
+        assert robot_mdl.as_dict() == expected_dict
     # print(robot_mdl.as_dict())
-    # with open("test_robot_param_export.yaml","w") as f:
-    #     yaml.safe_dump(robot_mdl.as_dict(), f, sort_keys=False)
+
+    if generate_oracle:
+        print("dumping the extracted parameters to "+str(expected_res_fpath.resolve()))
+        with open(expected_res_fpath,"w") as f:
+            yaml.safe_dump(robot_mdl.as_dict(), f, sort_keys=False, default_flow_style=None)
+            # https://stackoverflow.com/questions/23716531/python-yaml-dump-format-list-in-other-yaml-format
 
 
 if __name__ == "__main__":
-    test_compositional_asdict()
+    test_compositional_asdict(generate_oracle=True)
